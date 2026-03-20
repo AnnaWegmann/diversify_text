807487560b78aa8a89c6e75af6c09af6942227c7
AnnaWegmann <anna@prinzip.com>
2026-03-20 18:15:37 +0100

    - remove styles tinystyler

--- diversify_text/method/tinystyler/__init__.py
+++ diversify_text/method/tinystyler/__init__.py
@@ -1,6 +1,6 @@
 """TinyStyler method submodule."""
 
 from diversify_text.method.tinystyler.method import TinyStylerMethod
-from diversify_text.method.tinystyler.styles import DEFAULT_STYLE_BANK, DEFAULT_STYLES
+from diversify_text.styles import DEFAULT_STYLE_BANK, DEFAULT_STYLES
 
 __all__ = ["TinyStylerMethod", "DEFAULT_STYLE_BANK", "DEFAULT_STYLES"]
