bd54803e36ef5af6a4ce0943871a47439253537f
AnnaWegmann <anna@prinzip.com>
2026-08-31 14:52:22 +0200

    - shorten test

--- tests/test_tinystyler_bank.py
+++ tests/test_tinystyler_bank.py
@@ -4,21 +4,6 @@ from diversify_text.method.tinystyler import TinyStylerMethod
 
 
 class TestTinyStylerBank:
-    def test_default_n_prefix(self):
-        # The first five styles are what every default caller (n=5) gets.
-        assert list(TinyStylerMethod.style_bank)[:5] == [
-            "informal",
-            "formal",
-            "question",
-            "question_answer_forum",
-            "discussion_forum",
-        ]
-
-    def test_banks_are_disjoint(self):
-        assert not (
-            set(TinyStylerMethod.style_bank)
-            & set(TinyStylerMethod.unusual_style_bank)
-        )
 
     def test_all_styles_have_example_texts(self):
         for bank in (
